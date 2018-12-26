49d718f19ec1c04fc4609e3b4849a3d578d0bd2b
Inteag <43320068+Inteag@users.noreply.github.com>
2018-12-26 22:08:07 +0300

    laba 15.2

---
+++ laba15.2/laba15.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace laba15._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(func); //Создаем новый объект потока (Thread) 
+            myThread.Start(); //запускаем поток 
+            string str = "многопоточное";
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.WriteLine("Поток 1: " + str[i]);
+                Thread.Sleep(0);
+            }
+            Console.Read(); //Приостановим основной поток 
+        }
+        static void func()
+        {
+            string str = "программирование";
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.WriteLine("Поток 2: " + str[i]);
+                Thread.Sleep(0);
+            }
+
+        }
+    }
+}
