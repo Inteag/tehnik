e415bec04314b50f0241929ed2d35e16f22492d0
Inteag <43320068+Inteag@users.noreply.github.com>
2018-12-26 23:36:19 +0300

    Laba 8

---
+++ laba8/laba8/MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿/*
+ * Сделано в SharpDevelop.
+ * Пользователь: admin
+ * Дата: 23.11.2018
+ * Время: 20:45
+ * 
+ * Для изменения этого шаблона используйте Сервис | Настройка | Кодирование | Правка стандартных заголовков.
+ */
+namespace laba8
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Цвет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Обнулить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(140, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(518, 361);
+            this.panel1.TabIndex = 2;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel1MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainFormMouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Panel1MouseUp);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(8, 72);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(8, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Выбрать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 385);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "laba8";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel1MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainFormMouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Panel1MouseUp);
+            this.ResumeLayout(false);
+
+		}
+		private System.Windows.Forms.ColorDialog colorDialog1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button3;
+	}
+}
---
+++ laba8/laba8/MainForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace laba8
+{
+	public partial class MainForm : Form
+	{
+		Color CurrentColor = Color.Black; 
+		Point CurrentPoint; 
+		Point PrevPoint; 
+		bool isPressed; 
+		Graphics g;
+		public MainForm()
+		{
+			InitializeComponent();
+			g = panel1.CreateGraphics();
+			comboBox1.Items.Add("Прямоугольник");
+			comboBox1.Items.Add("Круг");
+		}
+		void Panel1MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			isPressed = true;
+			CurrentPoint = e.Location;
+		}
+		void Button1Click(object sender, System.EventArgs e)
+		{
+			DialogResult D = colorDialog1.ShowDialog();
+			if (D == System.Windows.Forms.DialogResult.OK)
+				CurrentColor = colorDialog1.Color;
+		}
+		void Button2Click(object sender, System.EventArgs e)
+		{
+			panel1.Refresh();
+		}
+		void ComboBox1SelectedIndexChanged(object sender, System.EventArgs e)
+		{
+			
+		}
+		void Button3Click(object sender, System.EventArgs e)
+		{
+			Pen p = new Pen(CurrentColor, 3);
+			Point point1 = new Point(50, 60);
+			Point point2 = new Point(100, 45);
+		    Point point3 = new Point(200, 30);
+		    Point[] curvePoints =
+		             {
+		                 point1,
+		                 point2,
+		                 point3
+		             };
+			if (comboBox1.SelectedIndex == 0)
+				g.DrawRectangle(p, 205, 125, 100, 100);
+			if (comboBox1.SelectedIndex == 1)
+				g.DrawEllipse(p, 205,125, 150, 150);
+
+		}
+		void Panel1MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			isPressed = false;
+		}
+		void MainFormMouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			if (isPressed)
+			{
+				PrevPoint = CurrentPoint;
+				CurrentPoint = e.Location;
+				paint_simple();
+			}
+		}
+		private void paint_simple()
+		{
+			Pen p = new Pen(CurrentColor);
+			g.DrawLine(p, PrevPoint, CurrentPoint);
+		}
+	}
+}
